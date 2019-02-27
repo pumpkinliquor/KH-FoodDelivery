b477757d2c37f59baaca0426532c6e5f0349a7d8
longbeom <wowow1859@naver.com>
2019-02-27 13:38:48 +0900

    사장님 / 회원 문의내역 분리

--- KHFoodDelivery/src/main/java/com/kh/food/admin/controller/qnaController.java
+++ KHFoodDelivery/src/main/java/com/kh/food/admin/controller/qnaController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class qnaController {
 
-	@RequestMapping("/admin/qnaList.do")
-	public String qnaList() {
-		return "admin/qnaList";
+	@RequestMapping("/admin/memberQnaList.do")
+	public String memberQnaList() {
+		return "admin/memberQnaList";
+	}
+	
+	@RequestMapping("/admin/ownerQnaList.do")
+	public String ownerQnaList() {
+		return "admin/ownerQnaList";
 	}
 }
