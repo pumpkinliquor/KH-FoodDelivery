2546112ee1d81c8e062ff2e40ee56499f07b2573
longbeom <wowow1859@naver.com>
2019-02-27 15:03:02 +0900

    DAO 에러 처리

--- KHFoodDelivery/src/main/java/com/kh/food/admin/model/dao/AppStoreDaoImpl.java
+++ KHFoodDelivery/src/main/java/com/kh/food/admin/model/dao/AppStoreDaoImpl.java
@@ -1,16 +1,18 @@
 package com.kh.food.admin.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AppStoreDaoImpl implements AppStoreDao {
-
+	
 	@Autowired
+	SqlSessionTemplate session;
 	
 	@Override
 	public int updateStoreConfirm(int no) {
-		return 0;
+		return session.update("store.updateStoreConfirm", no);
 	}
 
 }
