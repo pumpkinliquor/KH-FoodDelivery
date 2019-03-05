f520b307b7feb4899719580c2b5b9a64e9d6f54d
hwangjangwoon <hwangjangwoon@user1-PC>
2019-03-05 12:03:19 +0900

    리뷰, form 구현

--- KHFoodDelivery/src/main/java/com/kh/food/owner/onevsone/model/dao/OnevsOneDao.java
+++ KHFoodDelivery/src/main/java/com/kh/food/owner/onevsone/model/dao/OnevsOneDao.java
@@ -3,6 +3,8 @@ package com.kh.food.owner.onevsone.model.dao;
 import java.util.List;
 import java.util.Map;
 
+import com.kh.food.owner.onevsone.model.vo.OwnerQnaReview;
+
 public interface OnevsOneDao {
 	List<Map<String,String>> oneVSoneList();
 	List<Map<String,String>> myQnaList(String ownerId);
@@ -10,4 +12,6 @@ public interface OnevsOneDao {
 	int selectOwnerForm(String ownerId);
 //	int qnaFormEnd(Map<String, Object> qna);
 	List<Map<String,String>> qnaSearch(Map<String,String> map);
+	int qnaReviewForm(OwnerQnaReview oqr);
+	List<Map<String,String>> commentList(int qnaCode);
 }
