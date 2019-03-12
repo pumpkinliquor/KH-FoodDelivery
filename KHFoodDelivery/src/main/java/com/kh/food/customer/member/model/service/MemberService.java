989bf6ac3ce5d9b4199c789c016d7febae955d76
재웅 <user1@user1-PC>
2019-03-12 13:18:37 +0900

    commit

--- KHFoodDelivery/src/main/java/com/kh/food/customer/member/model/service/MemberService.java
+++ KHFoodDelivery/src/main/java/com/kh/food/customer/member/model/service/MemberService.java
@@ -14,5 +14,6 @@ public interface MemberService {
 	int checkNick(String nickName);
 	Member selectMember(String memberId);
 	int update(Member m);
-	List<Store> selectStore(String category);
+	List<Store> selectStore(String category,int cPage,int numPerPage);
+	int selectMenuCount();
 }
