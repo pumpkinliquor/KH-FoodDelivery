4da72b6746f0d3e10518b730f2b10266a0277588
longbeom <wowow1859@naver.com>
2019-03-15 12:49:44 +0900

    관리자 - 사장 문의 구현

--- KHFoodDelivery/src/main/java/com/kh/food/admin/controller/QnaMngController.java
+++ KHFoodDelivery/src/main/java/com/kh/food/admin/controller/QnaMngController.java
@@ -3,7 +3,6 @@ package com.kh.food.admin.controller;
 import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,18 +15,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.food.admin.model.service.QnaMngService;
 import com.kh.food.common.PagingFactory;
-import com.kh.food.owner.onevsone.model.vo.OwnerQna;
 import com.kh.food.qna.model.vo.MemberQna;
 import com.kh.food.qna.model.vo.MemberQnaReview;
+import com.kh.food.qna.model.vo.OwnerQna;
+import com.kh.food.qna.model.vo.OwnerQnaReview;
 
 @Controller
 /*@SessionAttributes({"keyword", "isRe", "category"})*/
@@ -43,14 +40,20 @@ public class QnaMngController {
 	public ModelAndView memberQnaList(@RequestParam(value="cPage", required=false, defaultValue="0") int cPage) {
 		ModelAndView mv = new ModelAndView();
 		int numPerPage = 5;
-		int count = service.selectMemberQnaCount();
+		int count = service.selectMemberQnaCount();		
 		// 회원 문의 리스트
 		List<MemberQna> mqList = service.selectMemberQnaList(cPage, numPerPage);
 		// 문의 날짜 포맷 (패턴 : yyyy-MM-dd)
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		for(int i = 0; i < mqList.size(); i++) {
 			mqList.get(i).setFormatWriteDate(df.format(mqList.get(i).getWriteDate()));
-		}		
+		}
+		
+		for(MemberQna q : mqList) {
+			logger.debug("" +q);
+		}
+		
+		logger.debug("count :" + count + " cPage : " + cPage + "numPerPage : " + numPerPage);
 		
 		mv.addObject("mqList", mqList);		
 		mv.addObject("pageBar", PagingFactory.getPageBar(count, cPage, numPerPage, "/food/admin/memberQnaList.do"));
@@ -185,21 +188,163 @@ public class QnaMngController {
 		return mv;
 	}
 	
-	// 사장님 문의 내역 리스트
+	// 사장 문의 내역 리스트
 	@RequestMapping("/admin/ownerQnaList.do")
 	public ModelAndView ownerQnaList(@RequestParam(value="cPage", required=false, defaultValue="0") int cPage) {
 		ModelAndView mv = new ModelAndView();
 		int numPerPage = 5;
+		int count = service.selectOwnerQnaCount();
 		// 사장 문의 리스트
 		List<OwnerQna> oqList = service.selectOwnerQnaList(cPage, numPerPage);
 		// 문의 날짜 포맷 (패턴 : yyyy-MM-dd)
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		for(int i = 0; i < oqList.size(); i++) {
 			oqList.get(i).setFormatWriteDate(df.format(oqList.get(i).getWriteDate()));
-		}
+		}		
 		
 		mv.addObject("oqList", oqList);		
+		mv.addObject("pageBar", PagingFactory.getPageBar(count, cPage, numPerPage, "/food/admin/ownerQnaList.do"));
 		mv.setViewName("admin/ownerQnaList");
+		return mv;
+	}
+	
+	// 사장 문의 검색
+	@RequestMapping("/admin/searchOwnerQna.do")
+	public ModelAndView searchOwnerQna(@RequestParam(value="keyword", defaultValue=" ") String keyword,
+										@RequestParam(value="isRe", defaultValue="3") String isRe,
+										@RequestParam(value="category", defaultValue="전체") String category,
+										@RequestParam(value="isFirst", defaultValue="0") int isFirst,
+										@RequestParam(value="cPage", required=false, defaultValue="0") int cPage,
+										HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+		
+		// 한글 인코딩
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		response.setCharacterEncoding("UTF-8");
+		
+		
+		ModelAndView mv = new ModelAndView();
+		int numPerPage = 5;
+		
+		logger.debug("키워드 : " + keyword);
+		logger.debug("isRe : " + isRe);
+		logger.debug("카테고리: " + category);
+		logger.debug("isFirst: " + isFirst);
+		
+		Map map1 = new HashMap();
+		if(isFirst == 1) {						// 처음 검색했을 때 map 객체 안에 조건 저장	
+			map1.put("keyword", keyword);
+			map1.put("isRe", isRe);
+			map1.put("category", category);
+			session.setAttribute("map1", map1);	// 맵을 세션에 저장
+		} else {								
+			map1 = (Map)session.getAttribute("map1");		// 검색 후 페이징 했을 땐 세션에서 조건을 가져옴
+		}
+		
+		List<OwnerQna> oqList = service.searchOwnerQna(map1, cPage, numPerPage);
+		int count = service.selectSearchOwnerQnaCount(map1);
+		// 문의 날짜 포맷 (패턴 : yyyy-MM-dd)
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		for(int i = 0; i < oqList.size(); i++) {
+			oqList.get(i).setFormatWriteDate(df.format(oqList.get(i).getWriteDate()));
+		}
+		
+		
+		logger.debug("count :" + count);
+		for(OwnerQna q : oqList) {
+			logger.debug(""+  q);
+		}
+		
+				
+		mv.addObject("oqList", oqList);		
+		mv.addObject("pageBar", PagingFactory.getPageBar(count, cPage, numPerPage, "/food/admin/searchOwnerQna.do"));
+		mv.setViewName("admin/ownerQnaList");				
+		return mv;
+	}
+	
+	// 사장 문의 보기
+	@RequestMapping("/admin/ownerQnaView.do")
+	public ModelAndView ownerQnaView(@RequestParam("no") int no) {
+		ModelAndView mv = new ModelAndView();
+		
+		// 문의 글 
+		OwnerQna oq = service.selectOwnerQna(no);
+		// 문의 날짜 포맷
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		oq.setFormatWriteDate(df.format(oq.getWriteDate()));
+		
+		// 문의 답변
+		try {
+			OwnerQnaReview oqr = service.selectOwnerQnaReview(no);
+			// 답변 날짜 포맷
+			oqr.setFormatWriteDate(df.format(oqr.getWriteDate()));
+			mv.addObject("oqr", oqr);
+		} catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+		
+		mv.addObject("oq", oq);
+		mv.setViewName("admin/ownerQnaView");
+		return mv;
+	}
+	
+	// 사장 문의 댓글 등록
+	@RequestMapping("/admin/insertOwnerQnaRe.do")
+	public ModelAndView insertOwnerQnaRe(@RequestParam("context") String context,
+										  @RequestParam("qnaNo") int no,
+			HttpServletRequest request, HttpServletResponse response) {
+		// 한글 인코딩
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		response.setCharacterEncoding("UTF-8");
+		
+		ModelAndView mv = new ModelAndView();
+		Map map = new HashMap();
+		map.put("context", context);
+		map.put("no", no);
+		
+		int result = service.insertOwnerQnaReview(map);		
+		
+		mv.setViewName("redirect:/admin/ownerQnaView.do?no=" + no);	
+		
+		return mv;
+	}
+
+	// 사장 문의 답변 삭제
+	@RequestMapping("/admin/deleteOwnerQnaReview.do")
+	public ModelAndView deleteOwnerQnaReview(@RequestParam("no") int no) {
+		ModelAndView mv = new ModelAndView();		
+		service.deleteOwnerQnaReview(no);
+
+		mv.setViewName("redirect:/admin/ownerQnaView.do?no=" + no);
+		return mv;
+	}	
+	
+	// 사장 문의 답변 수정
+	@RequestMapping("/admin/updateOwnerQnaReview.do")
+	public ModelAndView updateOwnerQnaReview(@RequestParam("no") int no, @RequestParam("updateContext") String context) {
+		ModelAndView mv = new ModelAndView();
+		Map map = new HashMap();
+		map.put("no", no);
+		map.put("context", context);
+		service.updateOwnerQnaReview(map);		
+		mv.setViewName("redirect:/admin/ownerQnaView.do?no=" + no);
+		return mv;
+	}	
+	
+	// 사장 문의글 삭제
+	@RequestMapping("/admin/deleteOwnerQna.do")
+	public ModelAndView deleteOwnerQna(@RequestParam("no") int no) {
+		ModelAndView mv = new ModelAndView();
+		
+		service.deleteOwnerQna(no);
+		mv.setViewName("redirect:/admin/ownerQnaList.do");
 		return mv;
 	}
 }
