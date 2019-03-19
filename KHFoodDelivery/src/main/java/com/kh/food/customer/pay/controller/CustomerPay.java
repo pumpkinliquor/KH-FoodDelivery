bf6f281dc2d0d4a77c09c6d70de635c2e6fd41ce
jeongbin <jeongbin@DESKTOP-V5UI6KT>
2019-03-19 09:13:14 +0900

    커밋

--- KHFoodDelivery/src/main/java/com/kh/food/customer/pay/controller/CustomerPay.java
+++ KHFoodDelivery/src/main/java/com/kh/food/customer/pay/controller/CustomerPay.java
@@ -1,20 +1,29 @@
 package com.kh.food.customer.pay.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.food.customer.pay.service.CustomerPayService;
 
 @Controller
 public class CustomerPay {
 
-	/*
-	 * @Autowired CustomerPayService service;
-	 */
 	
-	@RequestMapping("/customer/pay.do")
-	public String customerPay() {
-		return "customer/pay";
-	}
+	 @Autowired CustomerPayService service;
+	 
+	
+	/*
+	 * @RequestMapping("/customer/pay.do") public ModelAndView customerPay(int
+	 * totalPrice, String title) { ModelAndView mv= new ModelAndView();
+	 * Map<String,String> map=service.pay(); mv.addObject("totalPrice",totalPrice);
+	 * mv.addObject("title",title); mv.setViewName("/customer/payEnd");
+	 * 
+	 * return mv; }
+	 */
 	
 	@RequestMapping("/customer/payEnd.do")
 	public String customerPayEnd() {
